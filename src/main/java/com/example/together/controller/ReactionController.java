fa311ae9e466b29dc73e6748014437f3a7bf1ece
Hobao2k2 <baonamkka@gmail.com>
2025-01-01 23:16:30 +0700

    update reaction

--- src/main/java/com/example/together/controller/ReactionController.java
+++ src/main/java/com/example/together/controller/ReactionController.java
@@ -2,14 +2,13 @@ package com.example.together.controller;
 
 import com.example.together.dto.request.LikeArticleRequest;
 import com.example.together.dto.response.ApiResponse;
+import com.example.together.model.Reaction;
 import com.example.together.service.ReactionService;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/reaction")
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ReactionController {
     ReactionService reactionService;
+
     @PostMapping("/like-article")
     ApiResponse<String> likeArticle(@RequestBody LikeArticleRequest request) {
         return ApiResponse.<String>builder()
